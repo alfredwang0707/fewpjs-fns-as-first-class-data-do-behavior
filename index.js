e04d189ae702e5f4c9f395bffdf037a2c99f82a9
alfredwang0707 <alfredwang0707@gmail.com>
2020-12-09 21:09:47 -0500

    commit tree 6a33e77e93c67084c6afc4aadf8eae40c9eb4568

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+/* <h1 id="greeting"></h1> */
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
+
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  if (hour >12 && hour <17) return "Good Afternoon"
+
+  
+}
+
+// try {
+//   var message;
+//   message = document.getElementById("greeting");
+//   message.innerHTML = "" ;
+//   const hour = parseInt(timeStr, 10);
+//   if (hour <0 || hour > 24 )
+//   throw "must be between 0-24";
+// }
+// catch(err) {
+//   message.innerText = "Input is " + err;
+
+// }
